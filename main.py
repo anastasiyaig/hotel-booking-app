c40a3264e535b43789bc0d8ccd425e39edbb7ad0
Anastasiya Semenkevich <anastasija.ig@gmail.com>
2024-07-07 22:35:59 +0300

    feat: add spa selection

--- main.py
+++ main.py
@@ -21,6 +21,14 @@ class Hotel:
         df.to_csv("hotels.csv", index=False)
 
 
+class SpaHotel(Hotel):
+    def book_spa_package(self):
+        pass
+
+    def available(self) -> bool:
+        pass
+
+
 class ReservationTicket:
     def __init__(self, guest_name, hotel_object):
         self.guest_name = guest_name
@@ -29,6 +37,20 @@ class ReservationTicket:
     def generate(self):
         content = f"""
         Thank you for reservation, which is now confirmed.
+        Name: {self.guest_name}
+        Hotel name: {self.hotel.hotel_name}
+        """
+        return content
+
+
+class SpaReservationTicket:
+    def __init__(self, guest_name, hotel_object):
+        self.guest_name = guest_name
+        self.hotel = hotel_object
+
+    def generate(self):
+        content = f"""
+        Thank you for SPA reservation.
         Name: {self.guest_name}
         Hotel name: {self.hotel.hotel_name}
         """
@@ -60,16 +82,27 @@ class SecureCreditCard(CreditCard):
 print(df)  # print list of ids of hotels
 
 hotel_id = int(input("Enter the id of the hotel: "))
-hotel = Hotel(hotel_id)
+spa_hotel = SpaHotel(hotel_id)
 
-if hotel.available():
+if spa_hotel.available():
     credit_card = SecureCreditCard(card_number="1234123412341234")
     if credit_card.validate(expiration="12/26", holder="JOHN SMITH", cvc="123"):
         if credit_card.authenticate(given_password="mypass"):
-            hotel.book()
+            spa_hotel.book()
             name = input("Enter your name: ")
-            reservation_ticket = ReservationTicket(guest_name=name, hotel_object=hotel)
+            reservation_ticket = ReservationTicket(guest_name=name, hotel_object=spa_hotel)
             print(reservation_ticket.generate())
+            spa_choice = input("Do you want to add spa to your booking?")
+            match spa_choice.lower():
+                case "yes":
+                    if spa_hotel.available():
+                        spa_ticket = SpaReservationTicket(guest_name=name, hotel_object=spa_hotel)
+                        print(spa_ticket.generate())
+                    else:
+                        print("Sorry, we don't have any free slots now")
+                case "no":
+                    print("You can book spa any day of your stay on demand, please check the availability at the "
+                          "service desk")
         else:
             print("Your card authentication is not successful")
     else:
